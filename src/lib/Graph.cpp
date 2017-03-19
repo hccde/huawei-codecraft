e2c5ba2d95d1eafd8e66b9363bf0b9f2bd5b3919
hccde <1076663958@qq.com>
2017-03-19 23:45:18 +0800

    node class

--- src/lib/Graph.cpp
+++ src/lib/Graph.cpp
@@ -1,15 +1,29 @@
 #define DEBUG	true
 #ifdef DEBUG
 #include <iostream>
+using namespace std;
 #endif
 
-
+class Node{
+private:
+	int index;
+	int value;
+	// int upstream;
+	// int downstream;
+public:
+	Node(int indexcode,int val){
+		index = indexcode;
+		value = val;
+		// upstream = ups;
+		// downstream = downs;
+	}
+};
 
 
 #ifdef DEBUG
 
 int main(int argc,char** argv){
-	printf("hello\n");
+	Node n(2);
 	return 0;
 }
 
